d893e7cc25679c67df6daec98249c5f387714888
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-30 15:50:32 +0100

    Fix Linters Error

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,6 +1,6 @@
 class InventoryFood < ApplicationRecord
   belongs to :inventory
-  belongs to : food
+  belongs to :food
 
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
