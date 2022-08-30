48f36c7b91d784b9d8e675b120344c01e0851ff2
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-30 14:28:49 +0100

    Fixed Linters Error

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,7 +1,7 @@
 class Inventory < ApplicationRecord
   belongs to :user
 
-  has_many :foods through: :inventory_food
+  has_many :foods, through: :inventory_food
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
