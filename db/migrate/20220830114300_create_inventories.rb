719860e2afe15c13c31293475dee9e52adbcfd83
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-30 14:13:23 +0100

    Created Migration file for Inventories

---
+++ db/migrate/20220830114300_create_inventories.rb
@@ -0,0 +1,10 @@
+class CreateInventories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :inventories do |t|
+      t.string :name
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
