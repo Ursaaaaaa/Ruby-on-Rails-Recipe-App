ce7ba50b09e4316fad73b4fe04ea38081d3cec28
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-30 14:12:19 +0100

    Modified Config.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  root 'recipes#index'
   resources :recipes
   resources :food_recipes
   resources :foods
   resources :public_recipes
-  root 'recipes#index'
+  resources :inventory
+  resources :inventory_food
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
