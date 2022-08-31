bca076d17ba2067ee026650d293858b2c94a1ac1
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-31 14:38:11 +0100

    Modified InventoriesController

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,2 +1,38 @@
-class RecipesController < ApplicationController
+class InventoriesController < ApplicationController
+  def index
+    @recipes = if current_user.nil?
+                     []
+                   else
+                     @current_user.recipes
+                   end
+  end
+  def new
+    @inventory = Inventory.new
+  end
+
+  def show
+    @inventory = Inventory.find(params[:id])
+  end
+
+  def create
+    user = current_user
+    inventory = Inventory.new(params.require(:inventory).permit(:name))
+    
+    respond_to do |format|
+      format.html do
+        if inventory.save
+          flash[:success] = 'Inventory saved successfully'
+          redirect_to invetories_url
+        else
+          flash[:error] = 'Error: Inventory could not be saved'
+          redirect_to new_inventory_url
+        end
+      end
+    end
+  end
+
+  private
+  def inventory_params
+    params.require(:inventory).permit(:name, :description)
+  end
 end
