a534732629a6eeb4b2a6e80844982796573a29ce
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-01 12:12:06 +0100

    Modified Inventory Controller

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,11 +1,12 @@
 class InventoriesController < ApplicationController
   def index
-    @recipes = if current_user.nil?
+    @inventories = if current_user.nil?
                      []
                    else
-                     @current_user.recipes
+                     @current_user.inventories
                    end
   end
+
   def new
     @inventory = Inventory.new
   end
@@ -15,24 +16,31 @@ class InventoriesController < ApplicationController
   end
 
   def create
-    user = current_user
-    inventory = Inventory.new(params.require(:inventory).permit(:name))
-    
+    @user = current_user
+    @inventory = Inventory.new(inventory_params)
+    @inventory.user_id = @user.id
     respond_to do |format|
       format.html do
-        if inventory.save
+        if @inventory.save
           flash[:success] = 'Inventory saved successfully'
-          redirect_to invetories_url
+          redirect_to inventories_path
         else
           flash[:error] = 'Error: Inventory could not be saved'
-          redirect_to new_inventory_url
+          redirect_to new_inventory_url(@user)
         end
       end
     end
   end
 
+  def destroy
+    @inventory = Inventory.find(params[:id])
+    @inventory.destroy
+    flash[:notice] = 'Inventory was deleted successfully.'
+    redirect_to inventories_path
+  end
+
   private
   def inventory_params
-    params.require(:inventory).permit(:name, :description)
+    params.require(:inventory).permit(:name, :string)
   end
 end
