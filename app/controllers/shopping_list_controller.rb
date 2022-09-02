cae0b4f09f82ce82c0462ecfce1b9fa7c62f4cb4
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-02 07:31:13 +0100

    Modified ShopingListController

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,2 +1,22 @@
 class ShoppingListController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @total_price = 0
+    @foods = current_user.foods.includes(:food_recipes).map do |food|
+      next if food.food_recipes.empty?
+
+      total_quantity = food.food_recipes.sum(:quantity)
+      price = total_quantity * food.price
+      @total_price += price
+
+      {
+        name: food.name,
+        measurement_unit: food.measurement_unit,
+        price: food.price
+      }
+    end.compact
+    @recipes = current_user.recipes
+    @inventories = current_user.inventories
+  end
 end
