ce3b6e437b3d6b7beac126ddcc5d825402918f45
frank1738 <franklineosoro08@gmail.com>
2022-09-03 10:36:55 +0300

    fix users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :recipes, dependent: :destroy
-  has_many :foods, :inventories dependent: :destroy
+  has_many :foods, :inventories, dependent: :destroy
   validates :name, presence: true
-
